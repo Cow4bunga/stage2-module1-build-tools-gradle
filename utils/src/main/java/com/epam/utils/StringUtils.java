9530ec44a4891abf932d3b011cf67d38e30dab48
Ivan Levitsky <yavanyaprivet@gmail.com>
2022-11-26 22:37:00 +0300

    fixed utils again

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -4,8 +4,8 @@ import static org.apache.commons.lang3.math.NumberUtils.isCreatable;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        if (str == null || !isCreatable(str))
+        if (!isCreatable(str))
             return false;
-        return Integer.parseInt(str) > 0;
+        return !str.contains("-") && !str.equals("0") && !str.trim().equals("");
     }
 }
